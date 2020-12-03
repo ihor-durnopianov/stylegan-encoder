d105c8e0808a88f17377cf7252fdab2d4cae94ab
Ihor Durnopianov <ihor.durnopianov@outlook.com>
2020-12-03 19:44:51 +0200

    Clean up dependencies

--- Makefile
+++ Makefile
@@ -4,14 +4,14 @@ SHELL := /bin/bash
 
 .ONESHELL:
 
-# env:
-# 	python -m venv env
-# 	source env/bin/activate
-# 	pip install --upgrade pip
-# 	pip install wheel
-# 	pip install --requirement requirements-dev.txt
-# 	pip install --requirement requirements.txt
-# 	pip install --editable .
+env:
+	python -m venv env
+	source env/bin/activate
+	pip install --upgrade pip
+	pip install wheel
+	pip install --requirement requirements-dev.txt
+	pip install --requirement requirements.txt
+	pip install --editable .
 
 # test: env
 # 	source env/bin/activate
--- setup.py
+++ setup.py
@@ -12,7 +12,24 @@ setup(
     # packages=find_packages(),
     py_modules=["encoder"],
     python_requires='>=3.6',
-    install_requires=[],
+    install_requires=[
+        "Pillow",
+        "opencv-python",
+        "numpy",
+        "torch",
+        "torchvision",
+        "dlib",
+        # Keras==2.3.0
+        # tensorflow==2.1.0
+        "imutils",
+        "kornia",
+        "lpips-pytorch @ git+https://github.com/S-aiueo32/lpips-pytorch@d94711e",
+        "pytorch_stylegan_encoder @ git+https://github.com/ihor-durnopianov/pytorch_stylegan_encoder@0ea0f88",
+        "pytorch-ssim @ git+https://github.com/Po-Hsun-Su/pytorch-ssim@d6adfb5",
+        "style_gan @ git+https://github.com/ihor-durnopianov/style_gan@4686dcf",
+        # Indirect.  TODO: get rid of
+        "ipython",
+    ],
     # entry_points={
     #     "console_scripts": [
     #         # "deliverable-run=deliverable.__main__:main"
